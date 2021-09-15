c33181a2661d6f7d6dfa17e2b4836033b2f9211f
ichitac <78163643+ichitac@users.noreply.github.com>
2021-09-15 23:48:14 +0900

    Add files via upload

--- 20210911/qwerty.rb
+++ 20210911/qwerty.rb
@@ -1,12 +1,7 @@
-class Range
-    def value(item)
-        self.first(item).last
-    end
-end
 int = gets.chomp.split(/ /).map(&:to_i)
 str_range = ("a".."z")
 answer = String.new("")
 int.each do |i|
-    answer << str_range.value(i)
+    answer << str_range.first(i).last
 end
 print answer
