60fa4d1389c7677827f4ef368ecc88fd55a2ae2a
ichitac <78163643+ichitac@users.noreply.github.com>
2021-09-30 23:30:38 +0900

    Delete 20210911 directory

--- 20210911/Weather_Forecast.rb
+++
@@ -1,8 +0,0 @@
-day = gets.chomp.to_i
-weather_report = gets.chomp.split(//)
-
-if weather_report[day - 1] == "o"
-    puts "Yes"
-else
-    puts "No"
-end
--- 20210911/qwerty.rb
+++
@@ -1,7 +0,0 @@
-int = gets.chomp.split(/ /).map(&:to_i)
-str_range = ("a".."z")
-answer = String.new("")
-int.each do |i|
-    answer << str_range.first(i).last
-end
-print answer
